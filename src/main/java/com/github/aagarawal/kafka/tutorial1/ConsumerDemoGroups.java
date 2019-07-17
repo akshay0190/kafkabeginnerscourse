6bddbed8c357a0fc5578d48ac843dba2df0a603c
akshay <akki_akshay@yahoo.co.in>
2019-07-17 21:32:18 +0200

    Added group id

--- src/main/java/com/github/aagarawal/kafka/tutorial1/ConsumerDemoGroups.java
+++ src/main/java/com/github/aagarawal/kafka/tutorial1/ConsumerDemoGroups.java
@@ -28,6 +28,7 @@ public class ConsumerDemoGroups {
         properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
         properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG,groupId);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         //Create Consumer
