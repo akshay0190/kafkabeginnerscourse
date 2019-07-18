319c1051366bde45c879de0d6c6bbb2f8e58b099
akshay0190 <akki_akshay@yahoo.co.in>
2019-07-18 15:54:20 +0200

    Update

--- src/main/java/com/github/aagarawal/kafka/tutorial3/ElasticSearchConsumer.java
+++ src/main/java/com/github/aagarawal/kafka/tutorial3/ElasticSearchConsumer.java
@@ -1,18 +1,18 @@
 package com.github.aagarawal.kafka.tutorial3;
 
-import kafka.security.CredentialProvider;
+
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
+
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,29 +26,32 @@ import static org.elasticsearch.client.RequestOptions.DEFAULT;
  */
 public class ElasticSearchConsumer {
 
-    public static RestHighLevelClient createClient(){
+    public static RestHighLevelClient createClient() {
 
 //        https://guka1r63ex:d21setknns@kafka-cluster-5297324547.eu-west-1.bonsaisearch.net:443
         String hostname = "kafka-cluster-5297324547.eu-west-1.bonsaisearch.net";
-        String username ="guka1r63ex";
+        String username = "guka1r63ex";
         String password = "d21setknns";
-        final BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
-        credentialsProvider.setCredentials(AuthScope.ANY,
-                new UsernamePasswordCredentials(username,password));
-        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname,443,"https")).setHttpClientConfigCallback(httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY,new UsernamePasswordCredentials(username,password));
 
-        RestHighLevelClient client = new RestHighLevelClient(builder);
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname,443,"https")).setHttpClientConfigCallback(
+                httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider));
+
+       RestHighLevelClient client = new RestHighLevelClient(builder);
         return client;
     }
+
     public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
         String jsonString = "{ \"foo\": \"bar\" }";
         RestHighLevelClient client = createClient();
-       IndexRequest indexRequest;
+        IndexRequest indexRequest;
         indexRequest = new IndexRequest(
                 "twitter",
                 "tweets").
-        source(jsonString, XContentType.JSON);
+                source(jsonString, XContentType.JSON);
 
         IndexResponse indexResponse = client.index(indexRequest, DEFAULT);
         String id = indexResponse.getId();
